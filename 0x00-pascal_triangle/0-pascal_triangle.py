1ae08d3701b02a76926ef80f14f2a93fb79da36a
ianotee <oteeian500@gmail.com>
2024-05-27 14:06:33 +0000

    First commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script to that defines the pascals triangle."""
+
+
+def pascal_triangle(n):
+    """
+   This  return  will return a list okay.
+    """
+    triangle = []
+
+    
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+
+    return triangle
