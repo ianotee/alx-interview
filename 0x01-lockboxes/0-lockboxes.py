7ea9c1cbc3940ed40ac5be4987edddf2a3892782
ianotee <oteeian500@gmail.com>
2024-06-05 12:13:40 +0000

    fifth

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""A module for lockboxes.
-"""
+"""Module for lockboxes"""
 
 
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    opened_boxes = set([0])
-    unopened_boxes = set(boxes[0]).difference(set([0]))
-    while len(unopened_boxes) > 0:
-        boxId = unopened_boxes.pop()
-        if not boxId or boxId >= n or boxId < 0:
-            continue
-        if boxId not in opened_boxes:
-            unopened_boxes = unopened_boxes.union(boxes[boxId])
-            opened_boxes.add(boxId)
-    return n == len(opened_boxes)
+    """Checking the list of lists and the content
+       of a list and unlocking other lists
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
