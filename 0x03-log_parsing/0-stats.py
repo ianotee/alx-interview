e2dd037a11d12eb4a0df7f39f8916b5ddc17efb3
ianotee <oteeian500@gmail.com>
2024-06-20 15:13:22 +0000

    second commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,9 @@
 """Write a script that reads stdin line by line and computes metrics
     Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
     <status code> <file size>
+    (if the format is not this one, the line must be skipped)
+    After every 10 lines and/or a keyboard interruption
+    (CTRL + C), print these statistics from the beginning
 """
 
 
