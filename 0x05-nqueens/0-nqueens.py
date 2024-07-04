3841d80989e05b16210030afbb973bcdbad6e2e2
ianotee <oteeian500@gmail.com>
2024-07-04 08:44:29 +0000

    second commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,99 +1,61 @@
 #!/usr/bin/python3
+"""N Queens placement on NxN chessboard"""
 
-""" N queens Interview Questions """
 
 import sys
 
 
-def get_size():
-    """
-    Get the size of the board.
-    """
-    arguments = sys.argv
-    if len(arguments) != 2:
-        print('Usage: nqueens N')
-        sys.exit(1)
-    try:
-        n = int(arguments[1])
-        if n < 4:
-            print('N must be at least 4')
-            sys.exit(1)
-    except ValueError:
-        print('N must be an integer')
-        sys.exit(1)
-    return n
+def generate_solutions(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
 
-def unsafe_position(
-        board,
-        row,
-        col,
-        current) -> bool:
-    """
-    If the queen is in the same column, or in the same diagonal,
-    as any other queen, then it's unsafe
-
-    :param board: the current board
-    :type board: List[List[str]]
-    :param row: the row we're currently on
-    :type row: int
-    :param col: the column we're currently trying to place a queen in
-    :type col: int
-    :param current: the current row we are on
-    :type current: int
-    :return: True or False
-    """
-
-    if (board[row] == col) or \
-        (board[row] == col - row + current) or\
-            (board[row] == row - current + col):
-        return True
-    return False
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
 
-def print_board(board, n):
-    """
-    It takes a board and an n, and returns a list of the
-    coordinates of the queens on the board
-
-    :param board: a list of integers, where each integer
-    represents the column of the queen in that row
-    :param n: the number of queens
-    """
-
-    result = []
-
-    for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                result.append([i, j])
-    print(result)
-
-
-def fill_positions(board, row,  n):
-    """
-    For each row, try each column, and if it's safe, recurse on the next row
-
-    :param board: the chess board
-    :param row: the row we are currently on
-    :param n: the size of the board
-    """
-
-    if row == n:
-        print_board(board, n)
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
     else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
-        for j in range(n):
-            is_safe = True
-            for i in range(row):
-                if unsafe_position(board, i, j, row):
-                    is_safe = False
-            if is_safe:
-                board[row] = j
-                fill_positions(board, row + 1, n)
+
+def init():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        n = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return (n)
+
+
+def n_queens():
+
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
 
 
 if __name__ == '__main__':
-    board_size = get_size()
-    initial_list = [0] * board_size
-    fill_positions(initial_list, 0, board_size)
+    n_queens()
