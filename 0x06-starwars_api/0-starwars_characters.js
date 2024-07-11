4a00915b769d6530085c2ca7734327f66da4fa52
ianotee <oteeian500@gmail.com>
2024-07-11 10:23:40 +0000

    first commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 request(url, async function (error, response, body) {
   if (error) {
-    return console.log(error);
+    console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character in characters) {
+    for (const character of characters) {
       const res = await new Promise((resolve, reject) => {
-        request(characters[character], (err, res, html) => {
-          if (err) {
-            reject(err);
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
           } else {
             resolve(JSON.parse(html).name);
           }
